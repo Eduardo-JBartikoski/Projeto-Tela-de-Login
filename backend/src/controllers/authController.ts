1a4251ab4f68e83605636280ec2487407177167e
Eduardo-Bartikoski <eduardob.i207@gmail.com>
2026-02-18 17:23:55 -0300

    backend + prisma funcional

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "../lib/prisma";
 
-const prisma = new PrismaClient();
+
 
 
 //                       Registro usuário
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth";
 
-dotenv.config();
+
 
 const app = express();
 app.use(cors());
